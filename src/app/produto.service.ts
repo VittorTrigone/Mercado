45d3e391818e132a74f52221d1a75369e5c59455
VittorTrigone <Vittor.Trigone.Dias@gmail.com>
2023-05-16 14:49:42 -0300

    Mercado

--- src/app/produto.service.ts
+++ src/app/produto.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from './produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProdutos(): Observable<Client[]> {
+    let url = "http://localhost:3000/produtos";
+    return this.http.get<Client[]>(url);
+  }
 }
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../produto';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit {
 
+  produtos: Client[] = [];
+
+  constructor (private produtoService: ProdutoService){}
+
+  ngOnInit(): void {
+    this.loadProdutos();
+  }
+
+  loadProdutos() {
+    this.produtoService.getProdutos().subscribe(
+      {
+        next : data => this.produtos = data
+      }
+    );
+  }
 }
